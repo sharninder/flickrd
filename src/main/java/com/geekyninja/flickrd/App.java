f5133d12a47625af0814201f3a28c9a28e66764b
Sharninder <sharninder@gmail.com>
2016-06-09 00:07:55 +0530

    removed keys

--- src/main/java/com/geekyninja/flickrd/App.java
+++ src/main/java/com/geekyninja/flickrd/App.java
@@ -9,8 +9,8 @@ public class App {
 	public static void main(String[] args) {
 
 		// Add your own api key and api secret here
-		String apikey = "06d436f6bc9696aa3fdf92d89af25898";
-		String apisecret = "dca60b9cd77449a8";
+		String apikey = "";
+		String apisecret = "";
 		
 		
 	}
